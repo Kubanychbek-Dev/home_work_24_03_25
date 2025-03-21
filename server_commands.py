ee7c76406049dbe172e185aafb9acde6128af56b
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-03-21 11:58:02 +0300

    add create_table function

--- server_commands.py
+++ server_commands.py
@@ -1,3 +1,13 @@
 def create_database(name):
     command = fr"CREATE DATABASE {name}"
     return command
+
+
+def create_table(name):
+    command = fr"""CREATE TABLE {name}
+               (id INT PRIMARY KEY,
+               Name nvarchar(50),
+                Model nvarchar(50),
+                Year date,
+                 Price money);"""
+    return command
